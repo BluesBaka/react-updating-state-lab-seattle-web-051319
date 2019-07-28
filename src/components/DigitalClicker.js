212a11db907c9b9e1dac3445a450e6611b903ef2
Bluesbaka <badunnells@gmail.com>
2019-07-28 12:20:35 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+    handleClick = () => {
+      console.log(this.state)
+      this.setState(prevState => ({
+        timesClicked: prevState.timesClicked +1
+      }))
+    }
+
+  render() {
+    return(
+      <div>
+        <h1> Hello from Digital clicker</h1>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+            video: {
+              resolution: '1080p'
+          }
+        }
+          }
+  }
+
+    handleBitrateClick= () => {
+      console.log("my bitrate is broken")
+      this.setState({
+        settings:{
+        ...this.state.settings,
+        bitrate: 12
+      }
+      })
+    }
+
+    handleResolutionClick= ()=> {
+      console.log("why is everything so fuzzy?")
+      this.setState({
+        settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
+          resolution: '720p'
+            })
+          })
+
+        })
+    }
+
+
+
+
+  render(){
+    return(
+      <div>
+        <h1> Hello from youtube</h1>
+        <button className="resolution" onClick={this.handleResolutionClick}>AHHHHH, everything is blurry!</button>
+        <button onClick={this.handleBitrateClick} className="bitrate">Reset the State!</button>
+      </div>
+    )
+  }
+}
+export default YouTubeDebugger;
